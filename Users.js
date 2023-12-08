1c57f082b6af6a25560445ea53368c7b7e7dc1b6
test <annac101@nycstudents.net>
2023-12-08 01:56:02 -0500

    comments

--- Users.js
+++ Users.js
@@ -1,8 +1,8 @@
-fetch("https://jsonplaceholder.typicode.com/users")
-.then((response) => response.json())
-.then((json) => {
-    for (let x of json){
-    let element = document.createElement("li");
+fetch("https://jsonplaceholder.typicode.com/users")  //I am using fetch to make a GET request 
+.then((response) => response.json())   //It response back in binary so we put it through response.json to help us read it 
+.then((json) => { 
+    for (let x of json){ //goes through each element in the json array and creates an list item element for it 
+    let element = document.createElement("li"); 
     let ul = document.getElementById("list");
     element.innerText = "Name: "+x.name+"\n"+"Username: "+x.username;
     ul.appendChild(element);
